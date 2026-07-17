072048fb05c60105ecb5827d6d485bbda2c07ad4
Petar Kabashki <peter.kabashky@gmail.com>
2026-07-17 19:54:13 +0100

    wip

--- src/type.d.ts
+++ src/type.d.ts
@@ -2,3 +2,8 @@ declare module 'virtual:config' {
   const Config: import('astro-pure/types').ConfigOutput
   export default Config
 }
+
+declare module '*?raw' {
+  const content: string
+  export default content
+}
